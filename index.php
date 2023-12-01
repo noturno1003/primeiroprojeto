3412eb42dd7a99939517e88cee860a15699de065
noturno1003 <reisgabriel004gmail.com>
2023-11-30 21:32:10 -0300

    start

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+     <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>primeira criação</title>
+
+     <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <style>
+
+     body {background-image: url('https://i.pinimg.com/originals/fd/fe/94/fdfe94c7bdd0bf4afdd830e0b2c515e9.gif');
+     background: cover auto;
+     background-position: center auto;
+     width: 90vw;
+     height: 250vh;}
+
+      p {
+          color: white;
+          margin-top:10vh;
+          font-size:30px;
+     }
+      
+      h1 {
+          color: red;
+          margin-left: 25px;
+          margin-top: 20vh;
+      }
+
+      h2 {
+          color: white;
+          margin-left: 25px;
+          font-size: 30px;
+     }
+
+      h3 {
+          color: white;
+          margin-left: 35px;
+          font-size: 40px;
+     } 
+
+      h4 {
+          font-size: 60px;
+          color: gray;
+          margin-left: 300px;
+     }
+
+      h7 {
+          color: red;
+          font-size: 100px;
+          margin-left: 350px;
+     }
+
+      h8 {
+          color: pink;
+          font-size: 70px;
+          margin-left: 285px;
+     }
+
+      img {
+      max-width: 100%;
+      height: 350px;
+      display: block;
+      margin: 0 auto;
+     }
+
+      .contenedor-com-imagem {
+      background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUIdp_KogRiXR-PfNAv212r2bjTaFSfpAtAg&usqp=CAU');
+      background-size: cover;
+      background-position: center 110vh;
+     }
+
+      </style>
+      </head>
+      <body>
+    
+
+      <header>
+      <h7>Bem Vindo  </h7>
+      <h8>essa é uma página para teste</h8> 
+      <h2>estou testando uma parte chamada "background" que se resume no fundo da tela, estou praticando 
+      com alguns gifs e imagens de fundo mas ainda não aprendi a configurar direito.</h2>
+
+      <a href="https://youtu.be/-d5i2xb-i1E">
+      <img src="https://i.ytimg.com/an_webp/-d5i2xb-i1E/mqdefault_6s.webp?du=3000&sqp=CNzFo6sG&rs=AOn4CLDkWnDk4yfCKwFWKIvUdTupUxLYSw">
+      <h4> assista também!!!👆</h4>
+      </a>
+    
+      </header>
+
+      <header> 
+      <h1>A seguir tenho algumas noticias sobre a americanas(loja varejista do Brasil). </h1>
+
+      <h3> A lojas americanas está com um acordo de R$24 Bilhões, esse acordo foi divulgado na manhã do dia
+      27 de novembro de 2023, e as ações despencam logo que é notíciado o acordo.
+      
+      </h3>
+      
+      <h3>O movimento ocorre um dia após a companhia ter concretizado o acordo com os credores. Na manhã de segunda-feira (27), a 
+      companhia divulgou um fato relevante anunciando que havia finalizado um acordo com os principais credores.
+    
+      </h3>
+      <style>
+ 
+      </style>
+
+      <img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRP27cNfGFRBB45D3v5mgiXthLilS7mAJQhg&usqp=CAU">
+
+      <h3>Ainda em janeiro desse ano, as ações da americas alcaçaram um valor tão baixo que poucos acreditaram que iria subir ou até dobrar
+      a cotação aproximada de cada ação na época foi de R$ 0,64 e estava no inicio do ano, a seguir temos uma imagem ilustrando a queda.
+      </h3>
+
+      <img src="https://moneyinvest.com.br/wp-content/uploads/2023/01/amer.png">
+      <h3> E agora com essa diferença de valor muitos investidores acreditam que a americanas vai falir por completo, enquanto outra parte esmagadora está apenas observando e comprando alguns 
+      pedaços da empresa para que em um futuro talvez ela volte a reinar e dando um lucro significativo a todos que investiram na empresa.
+ 
+      <img src="https://comoinvestir.thecap.com.br/medias/2023/11/americanas-amer3-anuncia-acordo-com-credores-para-aporte-de-r-24-bilhoes-em-capitalizacao.webp">
+    
+      <h3>para mais informações recomendo analisar os dados do google finanças
+      o link vai estar logo abaixo!!!
+      </h3>
+      <a href="https://www.google.com/finance/quote/AMER3:BVMF?sa=X&ved=2ahUKEwjdnZzjk-uCAxWUMTUKHZ2DDwkQ_AUoAXoECAQQAw">
+      <h4> google finances.com</h4>
+      </a>
+      </header>
+ 
+      <header>
+      <h3>acompanhe também as ações da magazine luiza(MGLU3)</h3>
+      </header>
+
+      <header>
+      <a href="https://www.google.com/finance/quote/MGLU3:BVMF?sa=X&ved=2ahUKEwihwK_X6eyCAxVXqJUCHYgzDp0Q_AUoAXoECAIQAw">
+      <h4>Magazine Luiza</h4>
+      </a>
+      </header>
+
+      <main>
+      <h2> essa página é para fins de teste, sem grandes 
+      informações</h2>
+      </main>
+
+      <footer>
+      <p>ESSE PROJETO FOI CRIADO COM O INTUITO DE APRENDER COMO CONFIGURAR TEXTOS E IMAGENS. </p>
+      </footer>
+
+      <img src="https://i.pinimg.com/originals/40/32/e5/4032e5fb4cf8094a909c18970a5a6b54.gif">
+      
+      <h8> Agora irei a mimir pois estou  </h8>
+      <h8> com deveras sono potente :) </h8>
+      </body>
+      </html>
+       <html lang="pt-br">
+      <head>
+  
